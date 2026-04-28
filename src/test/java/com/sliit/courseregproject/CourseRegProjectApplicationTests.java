20b9716c8ec060e6a217ae167d9eaa494e70323d
IT25100067 <it25100067@my.sliit.lk>
2026-04-28 15:14:11 +0530

    First Commit

---
+++ src/test/java/com/sliit/courseregproject/CourseRegProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.courseregproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
